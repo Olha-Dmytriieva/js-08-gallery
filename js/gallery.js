9151528e97e90bcd1b89c5fa4c10332520988577
Olha-Dmytriieva <66797316+Olha-Dmytriieva@users.noreply.github.com>
2020-12-18 23:11:54 +0200

    slider

--- js/gallery.js
+++ js/gallery.js
@@ -38,64 +38,61 @@ pictures.forEach((item, indx) => {
 
 
 
-function mainPicFunction(event){
-    event.preventDefault()
-    if(event.target.nodeName !== 'IMG'){
-        return
-     }
+    function mainPicFunction(event){
+        event.preventDefault()
+        if(event.target.nodeName !== 'IMG'){
+            return
+        }
 
-      
-    mainPicEl.setAttribute('data-change', '0')
-    mainPicEl.src = event.target.dataset.picture;
-    mainPicEl.alt = `${event.target.description}`
-    
-    modalEl.classList.add('is-open');
-
-    
-   
-    
-}
-
-galleryEl.addEventListener('click', mainPicFunction)
-
-
-///modal open-close
-const closeButtonEl = document.querySelector('button[data-action="close-lightbox"]')
-const modalEl = document.querySelector('.js-lightbox')
-const overlayEl = document.querySelector('.lightbox__overlay')
-
-// function datasetRemoval(){
-//     if((modalEl.classList.contains('is-open')===false)){
         
-//         mainPicEl.src = "";
-//       } 
-// }
+        mainPicEl.setAttribute('data-change', '0')
+        mainPicEl.src = event.target.dataset.picture;
+        mainPicEl.alt = `${event.target.description}`
+        
+        modalEl.classList.add('is-open');
 
-closeButtonEl.addEventListener('click',()=>
-modalEl.classList.remove('is-open') )
-
-
-overlayEl.addEventListener('click',()=>
-modalEl.classList.remove('is-open') 
-
-
-
-)
-
-window.addEventListener('keydown', event =>{
-    if(event.code === 'Escape'){
-        mainPicEl.src = "";
-        modalEl.classList.remove('is-open') 
+        
     }
-// console.log(event.code)
 
-});
+    function sliderRight(event){
+        const indexRef = event.target.querySelector('img')
+        let calculator = indexRef.dataset.index;
+        
+        console.log(typeof calculator)
 
-// window.addEventListener('keydown', event =>{
-//     const calculator = '0';
-//     if(event.code === 'ArrowRight'){
-//         console.log(event.currentTarget)
-//     }
+    }
 
-// })
+
+    galleryEl.addEventListener('click', mainPicFunction)
+
+
+    ///modal open-close
+    const closeButtonEl = document.querySelector('button[data-action="close-lightbox"]')
+    const modalEl = document.querySelector('.js-lightbox')
+    const overlayEl = document.querySelector('.lightbox__overlay')
+
+    function datasetRemoval(){
+
+        modalEl.classList.remove('is-open');
+        mainPicEl.src = ""
+
+
+    }
+
+    closeButtonEl.addEventListener('click', datasetRemoval);
+    overlayEl.addEventListener('click', datasetRemoval);
+
+
+
+    window.addEventListener('keydown', sliderRight);
+
+    // window.addEventListener('keydown', event =>{
+    //     let calculator = 0;
+    //     if(event.code === 'ArrowRight'){
+    //         calculator = calculator + 1;
+
+        
+    //     } console.log(calculator)
+
+    // })
 
